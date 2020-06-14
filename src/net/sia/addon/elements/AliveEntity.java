3cf7ab4d9cb26200841ba0273e59c4944da0a71b
LynxEffqct <youtuber.kby@icloud.com>
2020-06-15 00:17:34 +0900

    Add files via upload

---
+++ src/net/sia/addon/elements/AliveEntity.java
@@ -0,0 +1,48 @@
+package net.sia.addon.elements;
+
+import org.bukkit.event.Event;
+import org.bukkit.craftbukkit.libs.jline.internal.Nullable;
+import org.bukkit.entity.Entity;
+
+import ch.njol.skript.lang.Expression;
+import ch.njol.skript.lang.SkriptParser.ParseResult;
+import ch.njol.skript.lang.util.SimpleExpression;
+import ch.njol.util.Kleenean;
+
+public class AliveEntity extends SimpleExpression<Boolean> {
+	private Expression<Entity> ex_e;
+
+	@Override
+	public Class<? extends Boolean> getReturnType() {
+		return Boolean.class;
+	}
+
+	@Override
+	public boolean isSingle() {
+		return true;
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public boolean init(Expression<?>[] arg0, int arg1, Kleenean arg2, ParseResult arg3) {
+		ex_e = (Expression<Entity>)arg0[0];
+		return true;
+	}
+
+	@Override
+	public String toString(@Nullable Event arg0, boolean arg1) {
+		return null;
+	}
+
+	@Override
+	@Nullable
+	protected Boolean[] get(Event arg0) {
+		Entity entity = (Entity)ex_e.getSingle(arg0);
+		if (entity.isDead() != true) {
+			return new Boolean[] { true };
+		} else {
+			return new Boolean[] { false };
+		}
+	}
+
+}
---
+++ src/net/sia/addon/elements/DelEntity.java
@@ -0,0 +1,33 @@
+package net.sia.addon.elements;
+
+import org.bukkit.event.Event;
+import org.bukkit.craftbukkit.libs.jline.internal.Nullable;
+import org.bukkit.entity.Entity;
+
+import ch.njol.skript.lang.Effect;
+import ch.njol.skript.lang.Expression;
+import ch.njol.skript.lang.SkriptParser.ParseResult;
+import ch.njol.util.Kleenean;
+
+public class DelEntity extends Effect {
+	private Expression<Entity> ex_e;
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public boolean init(Expression<?>[] arg0, int arg1, Kleenean arg2, ParseResult arg3) {
+		ex_e = (Expression<Entity>)arg0[0];
+		return true;
+	}
+
+	@Override
+	public String toString(@Nullable Event arg0, boolean arg1) {
+		return null;
+	}
+
+	@Override
+	protected void execute(Event arg0) {
+		Entity entity = (Entity)ex_e.getSingle(arg0);
+		entity.remove();
+	}
+
+}
